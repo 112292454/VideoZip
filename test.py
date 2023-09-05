d852e8344adc94b462731d2207e485ea83eb24b6
112292454 <1154459434@qq.com>
2023-09-06 02:17:35 +0800

    add 控制台log 文件metadata的记录信息 不重复处理的判断 覆写源文件的功能 覆写源文件与否的参数 读取ffprobe信息的默认值：无音轨、以及wmv/webm等缺信息的格式

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pprint
 import subprocess
@@ -8,42 +9,68 @@ from concurrent.futures import ThreadPoolExecutor
 
 from tools import *
 
-
-input_folder = "D:\资源\糖心VLOG\[小晗喵]/1"
-output_folder = "D:\资源\糖心VLOG\compressed_videos"
+input_folder = ""
+# output_folder = ""
 num_threads = 1  # 设置线程数
 
 ffmpeg = "ffmpeg"
 
+process_status={}
+
 
 def init():
     global ffmpeg
-    ffmpeg=get_os_program_name(ffmpeg)
-
-
-
+    ffmpeg = get_os_program_name(ffmpeg)
 
 
 # 定义处理单个视频的函数
 def process_video(video_file):
     try:
-        video_path=os.path.dirname(video_file)
-        video_file=os.path.basename(video_file)
+        video_path = os.path.dirname(video_file)
+        video_file = os.path.basename(video_file)
         base_name = os.path.splitext(video_file)[0]
-
         input_path = os.path.join(video_path, video_file)
         # 新的文件名，将后缀替换为.mp4
         output_path = os.path.join(video_path, f"compressed_{base_name}.mp4")
 
+        tags: dict = json.loads(read_mp4_tag(input_path, COMPRESSED_FLAG))
+        if tags.get("has_compressed"):
+            print(f'已处理过的文件，跳过:\t{input_path}')
+            return
+
         # 获取视频信息，可以使用 ffprobe 来获取
+        source_video_info = get_video_info(input_path)  # 调用获取视频信息函数
+        # todo:压缩率、处理变化、log
+        video_info = predict_video_info(source_video_info.copy())
 
-        video_info = get_video_info(input_path)  # 调用获取视频信息函数
-        #todo:压缩率、处理变化、log
-        video_info=predict_video_info(video_info)
+        should_be_modify = video_info['should_be_modify']
+        if not should_be_modify:
+            return
+        else:
+            result = run_command(input_path, output_path, video_info)
+            if result.returncode == 0:
+                save_file_logs(source_video_info, video_info, output_path)
+                if args.overwrite:
+                    if delete_file(input_path) and rename_file_remove_prefix(output_path, "compressed_"):
+                        # 不知道应不应该将重命名的换成指定两个文件名的方式，而不是删除前缀
+                        process_status[input_path] = "Succeed"
+                else:
+                    process_status[input_path] = "Succeed"
 
-        run_command(input_path, output_path, video_info)
+
+            else:
+                process_status[input_path] = "Failed"
+
     except Exception as e:
+        print('process failed : ' + video_file)
         traceback.print_exc()
+
+
+def save_file_logs(source_video_info, video_info, video_path):
+    logs = {"has_compressed": True}
+    logs["source_video_info"] = json.dumps(source_video_info, indent=2)
+    logs["video_info"] = json.dumps(video_info, indent=2)
+    write_mp4_tag(video_path, COMPRESSED_FLAG, json.dumps(logs, indent=2))
 
 
 def run_command(input_path, output_path, video_info):
@@ -56,160 +83,161 @@ def run_command(input_path, output_path, video_info):
         '-i', input_path,  # 输入文件路径
         '-c:v', video_info['video_codec_name'],  # 视频编码器
         # '-preset', 'fast',
-        '-crf', str(28),  # 264默认23，265默认28。越低越清晰，越大.每差6，体积翻倍
+        '-crf', str(args.crf),  # 264默认23，265默认28。越低越清晰，越大.每差6，体积翻倍
         '-maxrate', str(video_info['video_bit_rate']),  # 视频比特率
-        # '-bufsize', str(video_info['video_bit_rate']*8),
-        # '-vf', f'scale={width}:{height}',  # 调整分辨率
-        # '-r', str(video_info['frame_rate']),  # 输出帧率
+        '-bufsize', str(video_info['video_bit_rate'] * 8),
+        '-vf', f'scale={width}:{height}',  # 调整分辨率
+        '-r', str(video_info['frame_rate']),  # 输出帧率
         '-c:a', 'aac',  # 音频编码器为 AAC
-        # '-b:a', str(video_info['audio_bit_rate']),  # 音频比特率
-        # '-ar', str(video_info['audio_sample_rate']),  # 音频采样率
-        # '-y',
+        '-b:a', str(video_info['audio_bit_rate']),  # 音频比特率
+        '-ar', str(video_info['audio_sample_rate']),  # 音频采样率
+        '-y',
         output_path  # 输出文件路径
+        # input_path
+        # !!warn：此处直接覆盖源文件！使用之前最好做有备份，如果发现哪里有问题立刻停止。我目前测试了30g的视频没啥问题
+        # 如果保险一点可以使用上面注释掉的的output path，不直接使用input path来覆盖源文件
+        # 哦没事了，ffmpeg不允许覆盖源文件，只能py代码里手动删原本的，
     ]
     # 举例：调整分辨率为 720p，并降低码率
-    # compress_command = ['ffmpeg', '-i', input_path, '-vf', 'scale=1280:720', '-b:v', '1M', output_path]
     # 执行压缩命令
-    subprocess.run(compress_command)
+    return subprocess.run(compress_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 
 def predict_video_info(video_info):
-    M=1024*1024
+    M = 1024 * 1024
     # 从返回的字典中逐个声明变量
-    frame_rate          = video_info['frame_rate']                  # 视频帧率
-    video_bit_rate      = video_info['video_bit_rate']              # 视频码率
-    width               = video_info['width']                       # 视频帧宽度
-    height              = video_info['height']                      # 视频帧高度
-    duration            = video_info['duration']                    # 视频时长
-    video_codec_name    = video_info['video_codec_name']            # 视频编码方式
+    frame_rate = video_info['frame_rate']  # 视频帧率
+    video_bit_rate = video_info['video_bit_rate']  # 视频码率
+    width = video_info['width']  # 视频帧宽度
+    height = video_info['height']  # 视频帧高度
+    duration = video_info['duration']  # 视频时长
+    video_codec_name = video_info['video_codec_name']  # 视频编码方式
 
-    audio_bit_rate      = video_info['audio_bit_rate']              # 音频比特率
-    audio_sample_rate   = video_info['audio_sample_rate']           # 音频采样率
-    audio_codec_name    = video_info['audio_codec_name']            # 音频编码方式
+    audio_bit_rate = video_info['audio_bit_rate']  # 音频比特率
+    audio_sample_rate = video_info['audio_sample_rate']  # 音频采样率
+    audio_codec_name = video_info['audio_codec_name']  # 音频编码方式
 
-    should_be_modify    = video_info['should_be_modify']
-    file_size           = video_info['file_size']
-    file_name:str           = video_info['file_name']
-    file_path:str           = video_info['file_path']
+    should_be_modify = video_info['should_be_modify']
+    file_size = video_info['file_size']
+    file_name: str = video_info['file_name']
+    file_p: str = video_info['file_path']
 
-    if (file_name.startswith("compressed_")):
-        video_info['should_be_modify']=False
+    if file_name.startswith("compressed_"):
+        video_info['should_be_modify'] = False
         return
 
-
     # VR不作调整
-    if 'vr' in file_path.lower():
+    if 'vr' in file_p.lower():
         video_info['should_be_modify'] = False
         return video_info
 
     # 初音演唱会不作调整
-    if 'miku' in file_path.lower():
+    if 'miku' in file_p.lower():
         video_info['should_be_modify'] = False
         return video_info
 
-
-
     # 分辨率调整。8k、4k均降一档；2k待定要不要变1080p；720p以下的均提升到720p
-    shape={
-        '8k':(7680,4320),
-        '4k':(3840,2160),
-        '2k':(2560,1440),
-        "1080p":(1920,1080),
-        "720p":(1280,720)
+    shape = {
+        '8k': (7680, 4320),
+        '4k': (3840, 2160),
+        '2k': (2560, 1440),
+        "1080p": (1920, 1080),
+        "720p": (1280, 720)
     }
-    def size_judge(w,h,v1,v2):
-        return (v1*0.99<w<v1*1.01 and v2*0.99<h<v2*1.01) or (v1*0.99<h<v1*1.01 and v2*0.99<w<v2*1.01)
-    def adjust_trans(a,b,v1,v2):
-        if a>b : return v1,v2
-        else : return v2,v1
 
-    video_size:str='1080p'
-    if size_judge(width,height,*shape['8k']):  #8K
-        (video_info['width'],video_info['height'])=adjust_trans(width,height,*shape['4k'])
-        video_size='4k'
-    elif size_judge(width,height,*shape['4k']):
-        (video_info['width'],video_info['height'])=adjust_trans(width,height,*shape['2k'])
+    def size_judge(w, h, v1, v2):
+        return (v1 * 0.99 < w < v1 * 1.01 and v2 * 0.99 < h < v2 * 1.01) or (
+                v1 * 0.99 < h < v1 * 1.01 and v2 * 0.99 < w < v2 * 1.01)
+
+    def adjust_trans(a, b, v1, v2):
+        if a > b:
+            return v1, v2
+        else:
+            return v2, v1
+
+    video_size: str = '1080p'
+    if size_judge(width, height, *shape['8k']):  # 8K
+        (video_info['width'], video_info['height']) = adjust_trans(width, height, *shape['4k'])
+        video_size = '4k'
+    elif size_judge(width, height, *shape['4k']):
+        (video_info['width'], video_info['height']) = adjust_trans(width, height, *shape['2k'])
         video_size = '2k'
     # elif size_judge(width,height,*shape['2k']):
     #     (video_info['width'],video_info['height'])=adjust_trans(width,height,*shape['1080p'])
     #     video_size='1080p'
-    elif width<720 or height<720:
-        (video_info['width'],video_info['height'])=adjust_trans(width,height,*shape['720p'])
+    elif width < 720 or height < 720:
+        (video_info['width'], video_info['height']) = adjust_trans(width, height, *shape['720p'])
         video_size = '720p'
 
-
     # 帧率，归入到[30,60]
-    if frame_rate>60:
-        video_info['frame_rate']=60
-    elif frame_rate<30:
-        video_info['frame_rate']=30
-
-
+    if frame_rate > 60:
+        video_info['frame_rate'] = 60
+    elif frame_rate < 30:
+        video_info['frame_rate'] = 30
 
     # 比特率
-    br={
-        '8k': 32*M,
-        '4k': 16*M,
-        '2k': 8*M,
-        "1080p": 3*M,
-        "720p": 1.5*M
+    br = {
+        '8k': 32 * M,
+        '4k': 16 * M,
+        '2k': 8 * M,
+        "1080p": 3 * M,
+        "720p": 1.5 * M
     }
     # 只缩小，但是避免误操作，大小超过标称3倍的视频不做处理
-    if video_bit_rate>=br[video_size] and video_bit_rate<br[video_size]*3:
-        video_info['video_bit_rate']=br[video_size]
+    if br[video_size] <= video_bit_rate < br[video_size] * 3:
+        video_info['video_bit_rate'] = br[video_size]
     # 对于不足720p的放大到720p的码率，同时至少视频要5M，避免误操作
-    if  video_bit_rate<br['720p'] and file_size>5*M:
+    if video_bit_rate < br['720p'] and file_size > 5 * M:
         video_info['video_bit_rate'] = br['720p']
     # 60p视频帧率*1.5。https://zunzheng.com/news/archives/40648
     if frame_rate >= 59:
-        video_info['video_bit_rate']*=1.5
-
-
+        video_info['video_bit_rate'] *= 1.5
 
     # 编码方式
     # 画质 低比特用h265，高比特用vp9；https://www.macxdvd.com/mac-dvd-video-converter-how-to/vp9-vs-h265.htm
     # 编码时间 h264为vp9的1/4；https://bitmovin.com/vp9-vs-hevc-h265/
     # 大小 差不多，h265略小（0.2%到38%）。https://www.gumlet.com/learn/vp9-vs-h265/
-    if  size_judge(width,height,*shape['1080p']) or size_judge(width,height,*shape['720p']):
-        video_info['video_codec_name']='libx265'
+    # if size_judge(width, height, *shape['1080p']) or size_judge(width, height, *shape['720p']):
+    video_info['video_codec_name'] = 'libx265'
+    # args.crf = 28
     # else:
     #     video_info['video_codec_name']='libvpx-vp9'
 
     # 避免断点重复操作：关键参数不变的不修改
 
-
     # update
-    video_info['file_size']=video_info['duration']*video_info['video_bit_rate']/8
-
+    video_info['file_size'] = video_info['duration'] * video_info['video_bit_rate'] / 8
 
     return video_info
-
-
 
 
 init()
 
 if __name__ == '__main__':
-    args=get_args()
+    try:
+        args = get_args()
 
-    num_threads=args.thread_num
-    types=args.types.split(",")
-    input_folder=args.dir
+        num_threads = args.thread_num
+        types = args.types.split(",")
+        input_folder = args.dir
 
-    # 创建输出文件夹
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+        # 创建输出文件夹
+        # if not os.path.exists(output_folder):
+        #     os.makedirs(output_folder)
 
-    # 获取输入文件夹中的所有视频文件
-    video_files = walk_files(input_folder)
-    pprint.pp(video_files)
-    video_files=filter_files_by_types(video_files,types)
-
-
-    # 使用线程池并发处理视频压缩任务
-    with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        executor.map(process_video, video_files)
-    print("压缩完成！")
+        # 获取输入文件夹中的所有视频文件
+        video_files = walk_files(input_folder)
+        video_files = filter_files_by_types(video_files, types)
+        for file_path in video_files:
+            print(file_path)
+        # 使用线程池并发处理视频压缩任务
+        with ThreadPoolExecutor(max_workers=num_threads) as executor:
+            executor.map(process_video, video_files)
+        print("压缩完成！")
+        for key, value in process_status.items():
+            print(value, ":\t", key)
+    except Exception as e:
+        traceback.print_exc()
 
 # todo: windows适配：subprocess.run在win上需要写ffmpeg（或ffprobe）那个exe的全路径，不然报错，而linux直接ffmpeg就可以了。
 #  所以需要whereis ffmpeg自动获取到这个exe然后填进去
@@ -224,6 +252,7 @@ if __name__ == '__main__':
 
 # fix： 部分年代较早的视频转h265之后无法播放，原因未知，文件见附；h264可以播放，但压缩效果毕竟不好，并且会糊；vp9速度不到h265的一半，太慢了
 # 已提问题： https://stackoverflow.com/questions/77030749/when-using-ffmpeg-encode-to-hevc-but-got-rawvideo
+# ——疑似finish，设置成MP4就好了
 
 # fix： webm格式视频好像ffprobe获取信息不全，目前的写法会缺字段无法转换，可以考虑缺省。比如以下就会缺字段
 # ```
--- tools.py
+++ tools.py
@@ -4,10 +4,30 @@ import os
 import pprint
 import subprocess
 import sys
+from mutagen.mp4 import MP4, MP4Cover, MP4FreeForm
+
+COMPRESSED_FLAG = "COMP" # 标识是否已经压缩过的tag key
+# !!注意，tag只支持4字母识别  我看mutagen官网说有自由tag，但是没成功
+# https://mutagen.readthedocs.io/en/latest/api/mp4.html#module-mutagen.mp4
 
 ffprobe="ffprobe"
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dir', type=str, required=True,
+                        help='to processed video dir')
+    parser.add_argument('-t', '--types', type=str, default='mp4,avi,webm,mov,mpg,m4a,m4v,mpeg,wmv',
+                        help='to processed video type')
+    parser.add_argument('-thread', '--thread_num', type=int, default='3',
+                        help='thread num')
+    parser.add_argument('-crf', '--crf', type=int, default='22',
+                        help='crf level; no loss in {h264-19,h265-22,avi-22}, default in {h264-23,h265-28,avi-30}')
+    parser.add_argument('-O', '--overwrite', action='store_true',
+                        help='overwrite compressed file on source file,default is not, and named it as "compressed_xx"')
+
+    args = parser.parse_args()
+    return args
 
 # 获取文件夹内所有非目录文件
 def walk_files(root_dir):
@@ -35,16 +55,6 @@ def filter_files_by_types(file_list, file_types):
 
     return filtered_files
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--dir', type=str, required=False,
-                        help='to processed video dir')
-    parser.add_argument('-t', '--types', type=str, default='mp4,avi,webm,mov,mpg,m4a,m4v,mpeg,wmv ',
-                        help='to processed video type')
-    parser.add_argument('-thread', '--thread_num', type=int, default='4',
-                        help='thread num')
-    args = parser.parse_args()
-    return args
 
 def get_os_program_name(name):
     if sys.platform.startswith('win'):
@@ -67,7 +77,7 @@ def get_os_program_name(name):
     return name
 
 def get_video_info(video_path):
-    pprint.pp(video_path)
+    # pprint.pp(video_path)
     ffprobe_cmd = [ffprobe, '-v', 'error', '-show_entries',
                        'stream=duration,r_frame_rate,bit_rate,width,height,codec_name:stream=codec_name,bit_rate:stream=sample_rate',
                        '-of', 'json', video_path]
@@ -79,18 +89,31 @@ def get_video_info(video_path):
 
 
     video_stream = info_dict['streams'][0]
-    audio_stream = info_dict['streams'][1]
+    audio_stream = info_dict['streams'][1]if len(info_dict['streams'])>1 else {}
 
-    frame_rate = eval(video_stream['r_frame_rate'])  # 视频帧率
-    video_bit_rate = int(video_stream['bit_rate'])  # 视频码率
-    width = int(video_stream['width'])  # 视频帧宽度
-    height = int(video_stream['height'])  # 视频帧高度
-    duration = float(video_stream['duration'])  # 视频时长
-    video_codec_name = video_stream['codec_name']  # 视频编码方式
+    # frame_rate = eval(video_stream['r_frame_rate'])  # 视频帧率
+    # video_bit_rate = int(video_stream['bit_rate'])  # 视频码率
+    # width = int(video_stream['width'])  # 视频帧宽度
+    # height = int(video_stream['height'])  # 视频帧高度
+    # duration = float(video_stream['duration'])  # 视频时长
+    # video_codec_name = video_stream['codec_name']  # 视频编码方式
+    #
+    # audio_bit_rate = int(audio_stream['bit_rate'])  # 音频比特率
+    # audio_sample_rate = int(audio_stream['sample_rate'])  # 音频采样率
+    # audio_codec_name = audio_stream['codec_name']  # 音频编码方式
 
-    audio_bit_rate = int(audio_stream['bit_rate'])  # 音频比特率
-    audio_sample_rate = int(audio_stream['sample_rate'])  # 音频采样率
-    audio_codec_name = audio_stream['codec_name']  # 音频编码方式
+    # =============================由于一些wmv可能缺属性、或者有的视频没有音轨，因此设定默认值，可能会有bug======================
+
+    frame_rate = float(video_stream.get('frame_rate', 30.0))  # 视频帧率，默认值为30.0
+    video_bit_rate = int(video_stream.get('video_bit_rate', 2521927))  # 视频码率，默认值为2521927
+    width = int(video_stream.get('width', 1080))  # 视频帧宽度，默认值为1080
+    height = int(video_stream.get('height', 1920))  # 视频帧高度，默认值为1920
+    duration = float(video_stream.get('duration', 61))  # 视频时长，默认值为61
+    video_codec_name = video_stream.get('video_codec_name', 'h264')  # 视频编码方式，默认值为'h264'
+
+    audio_bit_rate = int(audio_stream.get('audio_bit_rate', 321199))  # 音频比特率，默认值为321199
+    audio_sample_rate = int(audio_stream.get('audio_sample_rate', 48000))  # 音频采样率，默认值为48000
+    audio_codec_name = audio_stream.get('audio_codec_name', 'aac')  # 音频编码方式，默认值为'aac'
 
     file_size = os.path.getsize(video_path)  # 获取文件大小（以字节为单位）
     file_name = os.path.basename(video_path)  # 获取文件名（不包括路径）
@@ -115,7 +138,57 @@ def get_video_info(video_path):
 
     return video_info
 
-# pprint.pp(get_video_info("D:\\资源\\糖心VLOG\\[小晗喵]\\猫耳女仆让我来品尝主人的肉棒吧_幻想主人 肉棒入侵_AV棒蹂躏嫩穴1.mp4"))
+
+def delete_file(file_path):
+    """删除指定路径的文件（如果存在）"""
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return True
+    else:
+        return False
+
+def rename_file_remove_prefix(file_path, prefix):
+    """重命名文件并去除文件名中的指定前缀"""
+    if os.path.exists(file_path):
+        # 获取文件名（包括扩展名）
+        base_name = os.path.basename(file_path)
+
+        # 检查文件名是否以指定前缀开头
+        if base_name.startswith(prefix):
+            # 去除前缀并重命名文件
+            new_base_name = base_name.replace(prefix, "", 1)
+            new_file_path = os.path.join(os.path.dirname(file_path), new_base_name)
+            os.rename(file_path, new_file_path)
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def write_mp4_tag(file_path, tag_name, tag_value):
+    try:
+        mp4_file = MP4(file_path)
+        mp4_file[tag_name] = [tag_value]
+        mp4_file.save()
+        # print(f"成功将标签'{tag_name}'设置为'{tag_value}'")
+    except Exception as e:
+        # print(f"{file_path}写入标签时出错：{e}")
+        return
+
+def read_mp4_tag(file_path, tag_name):
+    try:
+        mp4_file = MP4(file_path)
+        if tag_name in mp4_file:
+            tag_value = mp4_file[tag_name][0]
+            # print(f"标签'{tag_name}'的值为: {tag_value}")
+            return tag_value
+        else:
+            # print(f"标签'{tag_name}'不存在于文件{file_path}中")
+            return "{}"
+    except Exception as e:
+        # print(f"{file_path}读取标签时出错：{e}")
+        return "{}"
+
 
 # pprint.pprint(filter_files_by_types(walk_files("D:/资源/nano"),["mp3"]))
 
